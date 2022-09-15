1ef2f28911072fa9ad79736485044ff78e446da5
Kindness Igweh <igwehkindness@gmail.com>
2022-09-14 18:17:56 -0700

    Task 4-6 C-Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 include <stdio.h>
 /**
- * main - Prints the size of any var types
+ * main - Prints the size of many var types
  *
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	char a;
 	int b;
