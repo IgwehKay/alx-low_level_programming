99a03b6e49da338ae01a8e1a50ece32640c520bc
Kindness Igweh <igwehkindness@gmail.com>
2022-09-15 21:30:10 -0700

    0x03. C - Debugging, Task 0-1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,29 +1,16 @@
 #include "main.h"
-<<<<<<< HEAD
-/**
- * main - prints positive or negative integer
- * Return: 0
- */
-int main(void)
-{
-    int i;
-    i = 0;
-    positive_or_negative(i);
-=======
 
 /**
-* main - tests function that prints if positive or negative 
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
 int main(void)
 {
-    int i;
+int i;
 
-    i = 0;
-    positive_or_negative(i);
-    
->>>>>>> e1b1684f3b10630f4a814f54fd2e00dc253ca878
-    return (0);
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
-
